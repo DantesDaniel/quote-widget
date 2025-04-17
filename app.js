b5b73e3dc82f9e9e5964b6c83fd42b37c5c96340
Daniel Aliu <danielaliu202@gmail.com>
2025-04-17 15:02:51 +0100

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+function getQuote(){
+    fetch("quote.json")
+    .then(res => res.json())
+    .then(data => {
+        const quoteObj = data[Math.floor(Math.random() * data.length)]
+        document.getElementById("quote").textContent = quoteObj.quote
+        document.getElementById("author").textContent = quoteObj.author
+    })
+    .catch(error => console.error("Error loading quotes:", error));
+}
+
+getQuote()
